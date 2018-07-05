8af84cfb0a38d28e55f74b7d48075232a2465ae3
雨夜 <kiras0518@gmail.com>
2018-07-05 11:38:58 +0800

    new file

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,61 @@
+class CandidatesController < ApplicationController
+    
+    before_action :find_candidate, only: [:edit, :update, :destroy, :vote]
+
+    def index
+        @candidate = Candidate.all
+    end
+
+    def new
+        @candidate = Candidate.new
+    end
+
+    def create
+        @candidate = Candidate.new(candidate_params)
+
+        if @candidate.save
+            redirect_to candidates_path, notice: "新增候選人成功!"
+        else
+            render :new
+        end
+    end
+
+    def edit
+        #把 id 是 params[:id] 的資料抓出來
+        #@candidate = Candidate.find(params[:id])
+    end
+
+    def update
+        #@candidate = Candidate.find_by(id: params[:id])
+  
+        if @candidate.update(candidate_params)
+          # 成功
+          redirect_to candidates_path, notice: "資料更新成功!"
+        else
+          # 失敗
+          render :edit
+        end
+    end
+
+    def destroy
+      #@candidate = Candidate.find_by(id: params[:id])
+      @candidate.destroy if @candidate
+      redirect_to candidates_path, notice: "資料已刪除!"
+    end
+
+    def vote
+        #@candidate = Candidate.find_by(id: params[:id])
+        @candidate.vote_logs.create(ip_address: request.remote_ip) if @candidate
+        redirect_to candidates_path, notice: "完成投票!" 
+    end
+
+    private
+    def candidate_params
+      params.require(:candidate).permit(:name, :age, :party, :politics)
+    end
+
+    def find_candidate
+      @candidate = Candidate.find_by(id: params[:id])
+    end
+
+end
---
+++ db/migrate/20180704073548_create_candidates.rb
@@ -0,0 +1,14 @@
+class CreateCandidates < ActiveRecord::Migration[5.2]
+  def change
+    create_table :candidates do |t|
+      t.string :name
+      t.string :party
+      t.integer :age
+      t.text :politics
+      t.integer :votes,default: 0
+      t.datetime :time
+
+      t.timestamps
+    end
+  end
+end
